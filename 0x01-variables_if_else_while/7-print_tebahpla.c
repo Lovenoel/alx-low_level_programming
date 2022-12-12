edd9331794432787d5bf6e0fae1cca32d4cea7e2
lovenoel <kinglovenoel@gmail.com>
2022-12-11 21:58:26 -0800

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Prints the lowercase alphabet in reverse
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char m;
+
+	m = 'z';
+	while
+		(m >= 'a') {
+			putchar(m);
+			m--;
+		}
+	putchar('\n');
+	return (0);
+}
