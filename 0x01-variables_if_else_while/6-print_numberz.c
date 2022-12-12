b4ee73cc09b023cba7bed9f4000331f036a3c717
lovenoel <kinglovenoel@gmail.com>
2022-12-11 21:27:33 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Prints all the single nmbers of base of base 10 starting with 0
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	a = 0;
+	while
+	       	(a < 10) {
+			putchar('a');
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
