102e66a1afa85221cab6361c6eba06db6cf9ce6d
lovenoel <kinglovenoel@gmail.com>
2022-12-15 22:02:17 -0800

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,13 +9,9 @@ void print_line(int n)
 {
 	int a;
 
-	if (n <= 0)
+	if (n > 0)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (a = 1; a <= n; a++)
+		for (a = 0; a <= n; a++)
 		{
 		_putchar('_');
 		}
