1e9ed8b436c8d84247dbf6f0172fb78241da2ad6
lovenoel <kinglovenoel@gmail.com>
2022-12-19 03:53:29 -0800

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the character
+ * Return: Length of a string
+ */
+int _strlen(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0'; index++)
+
+	return (index);
+}
