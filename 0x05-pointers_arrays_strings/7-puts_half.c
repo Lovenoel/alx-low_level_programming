3d81dc8b4d80149e3222610a73d613af9da7dcbd
lovenoel <kinglovenoel@gmail.com>
2022-12-19 08:27:36 -0800

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,12 +11,15 @@ void puts_half(char *str)
 	index = 0;
 	while (str[index] != '\0')
 		index++;
+
 	half = index / 2;
+
 	if (index % 2 == 1)
 		half++;
+
 	while (half < index)
 	{
-		_putchar(str[index]);
+		_putchar(str[half]);
 		half++;
 	}
 	_putchar('\n');
