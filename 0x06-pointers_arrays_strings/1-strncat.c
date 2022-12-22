210d8c5cb86dd98a142bf3f8037ba8753fa3613c
lovenoel <kinglovenoel@gmail.com>
2022-12-21 23:51:47 -0800

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings but add inputted
+ * number of bytes
+ * @dest: first string
+ * @src: second string
+ * @n: the integer
+ * Return: pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
