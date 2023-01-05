6185aa3b8166c5bd50a684318fa852622906e859
lovenoel <kinglovenoel@gmail.com>
2023-01-05 09:25:22 -0800

    Task 1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string
- * @s: the string
+ * _puts_recursion - prints a string recursively
+ * @s: the string parameter
  */
 void _puts_recursion(char *s)
 {
@@ -13,5 +13,7 @@ void _puts_recursion(char *s)
 	}
 	_putchar(*s);
 	s++;
-	_puts_recursion(s);
+	{
+		_puts_recursion(s);
+	}
 }
