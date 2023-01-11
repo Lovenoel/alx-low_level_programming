28b7c591ef53c686032d2f656b9c298ae4b93c9f
lovenoel <kinglovenoel@gmail.com>
2023-01-11 02:49:17 -0800

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,7 +2,16 @@
 
 /**
  * _memcpy - copies memory area
+ * @dest: the new menory area
+ * @n: the number of memory bytes
+ * @src: the source memory
+ * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		dest[a] = src[a];
+	return (dest);
 }
