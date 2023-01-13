a75d636773ce14ceab1be8fdedca9569e3aa1ec4
lovenoel <kinglovenoel@gmail.com>
2023-01-12 18:48:21 -0800

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,9 +14,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int size1, size2, i;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 	if (s1[size1] != '\0')
 	{
 		size1++;
